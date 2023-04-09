ced9bae88ff5cef6cc0e81dbce0c7b6f20853be8
FedorTselishchev <ikbenfedor@gmail.com>
2023-04-09 23:20:11 +0200

    Gemaakt met @KevinVanH  alles bijna af
    Gemaakt met @KevinVanH

---
+++ Kerstman-Fedor-Kevin/KerstmanPROG6-Fedor-Kevin/Controllers/GiftController.cs
@@ -0,0 +1,134 @@
+﻿using KerstmanPROG6_Fedor_Kevin.models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SantasWishlist.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace KerstmanPROG6_Fedor_Kevin.Controllers
+{
+    public class GiftController : Controller
+    {
+        private IGiftRepository _giftRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private GiftList _giftList;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public GiftController(IGiftRepository giftRepository, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _giftRepository = giftRepository;
+
+            _giftList = new GiftList();
+        }
+
+        [HttpPost]
+        //[Authorize]
+        public IActionResult AboutMe(string username, string behaviour, int age, string explanation)
+        {
+            ViewBag.Username = username;
+            ViewBag.Age = age;
+            ViewBag.Explanation = explanation;
+            ViewBag.Behaviour = behaviour;
+            ViewBag.Gifts = _giftRepository.GetPossibleGifts();
+
+            ViewBag.Errors = "";
+
+            if (age <= 0)
+            {
+                TempData["alertMessage"] = "vul een geldige leeftijd!";
+                return View("/Views/Home/Index.cshtml");
+            }
+            return View("/Views/Wishlist/Presents.cshtml", _giftList);
+        }
+
+        [HttpPost]
+        //[Authorize]
+        public async Task<IActionResult> Wishlist(List<string> gifts, int age, string behaviour, string username, string explanation, string extra)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            List<Gift> giftList = new List<Gift>();
+            ICollection<ValidationResult> results = new List<ValidationResult>();
+            List<string> errors = new List<string>();
+
+            foreach (var gift in _giftRepository.GetPossibleGifts())
+            {
+                foreach (var chosengift in gifts)
+                {
+                    if (gift.Name == chosengift)
+                    {
+                        giftList.Add(gift);
+                    }
+                }
+            }
+            _giftList.allGifts = giftList;
+            _giftList.Behaviour = behaviour;
+            _giftList.Age = age;
+            _giftList.user = user;
+            _giftList.giftRepository = _giftRepository;
+            _giftList.Extra = extra;
+
+            if (explanation != null)
+                _giftList.Explanation = explanation;
+
+            if (Validator.TryValidateObject(_giftList
+            , new ValidationContext(_giftList)
+            , results, true))
+            {
+                ViewBag.Username = username;
+                ViewBag.Age = age;
+                ViewBag.Explanation = explanation;
+                ViewBag.Behaviour = behaviour;
+                ViewBag.Gifts = _giftRepository.GetPossibleGifts();
+                ViewBag.ChosenGifts = gifts.Skip(1);
+
+                return View("/Views/Wishlist/WishlistOverview.cshtml");
+            }
+            else
+            {
+                foreach (var result in results.Distinct().ToList())
+                    if (result != null)
+                        errors.Add(result.ErrorMessage);
+
+                ViewBag.Errors = errors.Distinct().ToList();
+                ViewBag.Username = username;
+                ViewBag.Age = age;
+                ViewBag.Explanation = explanation;
+                ViewBag.Behaviour = behaviour;
+                ViewBag.Gifts = _giftRepository.GetPossibleGifts();
+
+                return View("/Views/Wishlist/Presents.cshtml");
+            }
+        }
+
+        [HttpPost]
+        //[Authorize]
+        public async Task<IActionResult> ConfirmAsync(List<string> gifts)
+        {
+            var user = await _userManager.GetUserAsync(User);//TODO
+
+            WishList wishList = new WishList();
+            wishList.Name = user.UserName;
+
+            foreach (var gift in _giftRepository.GetPossibleGifts())
+            {
+                foreach (var chosengift in gifts)
+                {
+                    if (gift.Name == chosengift)
+                    {
+                        wishList.Wanted.Add(gift);
+                    }
+                }
+            }
+
+            _giftRepository.SendWishList(wishList);
+            user.IsRegistered = true;
+            //update user
+
+            await _signInManager.SignOutAsync();
+            TempData["successMessage"] = "De kerstman heeft jouw verlanglijstje ontvangen!";
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ Kerstman-Fedor-Kevin/KerstmanPROG6-Fedor-Kevin/Controllers/SantaController.cs
@@ -0,0 +1,104 @@
+﻿using KerstmanPROG6_Fedor_Kevin.models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Text.RegularExpressions;
+
+namespace KerstmanPROG6_Fedor_Kevin.Controllers
+{
+    public class SantaController : Controller
+    {
+        public string[] names;
+        public SantaController @object;
+        public UserManager<ApplicationUser> _userManager;
+        public SignInManager<ApplicationUser> _signInManager;
+        public RoleManager<ApplicationUserRole> _roleManager;
+        
+        public SantaController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            RoleManager<ApplicationUserRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+        [HttpPost]
+        //[Authorize]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(ApplicationUser Input)
+        {
+            if (ModelState.IsValid)
+            {
+                names = Input.Name.Split(',');
+
+                for (int x = 0; x < names.Length; x++)
+                {
+                    names[x] = Regex.Replace(names[x], @"\s", "");
+                }
+                if (names.Length != names.Distinct().Count())
+                {
+                    TempData["alertMessage"] = "Er staan twee namen in de lijstje.";
+                    return Redirect("/");
+                }
+                if (ModelState.IsValid)
+                {
+                    for (int x = 0; x < names.Length; x++)
+                    {
+                        var user = new ApplicationUser { UserName = names[x], Name = names[x] };
+                        user.IsBehaved = Input.IsBehaved;
+                        user.IsRegistered = false;
+                        var role = new ApplicationUserRole { Role = "Child", Name = "Child" };
+
+                        var claim = new Claim("Role", "Child");
+
+                        ViewBag.Persons = Input.Name;
+                        ViewBag.Password = Input.Password.ToLower();
+                    }
+                    return View("/Views/Home/RegisterSuccess.cshtml");
+                }
+            }
+            return Redirect("/");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(ApplicationUser Input)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(Input.Name);
+                if (user.IsRegistered != true)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(Input.Name, Input.Password.ToLower(), false, lockoutOnFailure: false);
+
+                    if (result.Succeeded)
+                    {
+                        return LocalRedirect("/");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid");
+                        return View();
+                    }
+                }
+            }
+            TempData["alertMessage"] = "Je hebt je verlanglijstje al ingevuld! De kerstman is al voor je bezig.";
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
