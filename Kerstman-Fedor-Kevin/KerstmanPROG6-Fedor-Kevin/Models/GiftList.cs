2d237fdce4ec6026e033b4f04c909c2b0d0e5d89
FedorTselishchev <ikbenfedor@gmail.com>
2023-04-09 23:50:17 +0200

    suii

--- Kerstman-Fedor-Kevin/KerstmanPROG6-Fedor-Kevin/Models/GiftList.cs
+++ Kerstman-Fedor-Kevin/KerstmanPROG6-Fedor-Kevin/Models/GiftList.cs
@@ -23,6 +23,7 @@ namespace KerstmanPROG6_Fedor_Kevin.models
 
         public string Extra;
 
+        //TODO REFACTOR
         public IEnumerable<ValidationResult> Validate(ValidationContext Context)
         {
             // Get the number of gifts
@@ -31,7 +32,6 @@ namespace KerstmanPROG6_Fedor_Kevin.models
             // Check if the user is a volunteer
             bool isVolunteer = false;
 
-            // Rule 4
             if (user != null && user.UserName.ToLower() == "stijn") // To lower so either Stijn or stijn works
             {
                 foreach (var rule4i1 in allGifts)
@@ -42,19 +42,15 @@ namespace KerstmanPROG6_Fedor_Kevin.models
                     }
                 }
             }
-
             if (Behaviour != null)
             {
                 // Check for good behavior
                 if (Behaviour == "goodbehaviour")
                 {
-                    // Rule 5
                     if (Explanation != null && Explanation.ToLower() == "vrijwilligerswerk" && user.IsBehaved)
                     {
                         isVolunteer = true;
                     }
-
-                    // Rule 1
                     if (user.IsBehaved &&
                         (allGifts.Where(c => c.Category == GiftCategory.WANT).Count() > 3 ||
                          allGifts.Where(c => c.Category == GiftCategory.NEED).Count() > 3 ||
@@ -63,9 +59,7 @@ namespace KerstmanPROG6_Fedor_Kevin.models
                     {
                         yield return new ValidationResult("Have been nice, but do not choose too many gifts! (max 3)");
                     }
-
-                    //Rule 9
-                    else if (user.IsBehaved == true)
+                    if (user.IsBehaved == true)
                     {
                         if ((allGifts.Where(c => c.Category == GiftCategory.WANT).Count() == 0 ||
                             allGifts.Where(c => c.Category == GiftCategory.NEED).Count() == 0 ||
@@ -75,10 +69,6 @@ namespace KerstmanPROG6_Fedor_Kevin.models
                             yield return new ValidationResult("You have been nice! Choose at least 1 gift per category!");
                         }
                     }
-
-                    // Rule 1
-                    // Exception: Santa knows a kid has been bad, but the kid lied about it in about me.
-                    // Only 1 present can be chosen.       
                     if (user.IsBehaved == false &&
                         allGifts.Where(c => c.Category == GiftCategory.WANT ||
                                             c.Category == GiftCategory.NEED ||
@@ -88,51 +78,67 @@ namespace KerstmanPROG6_Fedor_Kevin.models
                         yield return new ValidationResult("You've been naughty AND been lying about it. You may ony choose 1 gift!");
                     }
                 }
-                // Rule 1
-                // Exception: A kid that has been bad may only choose 1 gift per category
                 else if (Behaviour == "badbehaviour" && !user.IsBehaved)
+                {
                     if (allGifts.Where(c => c.Category == GiftCategory.WANT).Count() > 1 ||
-                        allGifts.Where(c => c.Category == GiftCategory.NEED).Count() > 1 ||
-                        allGifts.Where(c => c.Category == GiftCategory.WEAR).Count() > 1 ||
-                        allGifts.Where(c => c.Category == GiftCategory.READ).Count() > 1 && !isVolunteer)
+                           allGifts.Where(c => c.Category == GiftCategory.NEED).Count() > 1 ||
+                           allGifts.Where(c => c.Category == GiftCategory.WEAR).Count() > 1 ||
+                           allGifts.Where(c => c.Category == GiftCategory.READ).Count() > 1 && !isVolunteer)
                         yield return new ValidationResult("Stout geweest, dat betekent maar max 1 cadeau per categorie!");
+                }
             }
             if (allGifts != null)
             {
-                // Rule 6, oblige underwear with lamp
                 foreach (var rule6gift in allGifts)
+                {
                     if (rule6gift.Name == "Nachtlampje")
+                    {
                         foreach (var rule6i2 in allGifts)
+                        {
                             if (rule6i2.Name != "Ondergoed")
+                            { 
                                 yield return new ValidationResult("Want a night lamp? You need underwear with that!");
-
-                // Rule 2, dont allow both knex and lego
+                            }
+                        }
+                            
+                    }
+                        
+                }
                 var lego = allGifts.Where(i => i.Name.ToLower().Contains("lego")).Count();
                 var knex = allGifts.Where(i => i.Name.ToLower().Contains("k`nex") ||
                                 i.Name.ToLower().Contains("knex")).Count();
 
                 if (lego >= 1 && knex >= 1)
+                { 
                     yield return new ValidationResult("Both lego AND knex is unacceptable!");
+                }
 
-                // Rule 3, ony 1 gift alloweed that doesnt match age
-                var skipAgeCounter = allGifts.Where(i => giftRepository.CheckAge(i.Name) > Age).Count();
 
-                if (skipAgeCounter == 2)
-                    yield return new ValidationResult("You may only have 1 gift that does not match your age.");
-
-                // Rule 7, instrument requires earplugs
                 var match1 = allGifts.Where(x => x.Name == "Muziekinstrument" || x.Name == "Oordopjes").Count();
 
                 if (match1 != 0)
+                { 
                     yield return new ValidationResult("You cannot get a music instrument without oordopjes");
+                }
+                var skipAgeCounter = allGifts.Where(i => giftRepository.CheckAge(i.Name) > Age).Count();
+
+                if (skipAgeCounter == 2)
+                { 
+                    yield return new ValidationResult("You may only have 1 gift that does not match your age.");
+                }
+
             }
 
-            // Rule 8 Cant pick extra from gift list
             if (Extra != null)
+            {
                 foreach (var gift in giftRepository.GetPossibleGifts())
+                {
                     if (gift.Name.ToLower() == Extra.ToLower())
+                    {
                         yield return new ValidationResult("Cannot pick a gift from this list.");
-
+                    }
+                }
+            }
             yield return ValidationResult.Success;
         }
     }
