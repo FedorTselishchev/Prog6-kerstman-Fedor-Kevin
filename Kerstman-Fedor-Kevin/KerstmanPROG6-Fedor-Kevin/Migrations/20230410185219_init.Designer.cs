6dd15c3b76cd5f67c99720a9ea713c636c5e3950
Kevin <kevinskievanherwijnen@gmail.com>
2023-04-10 20:56:08 +0200

    yep database

--- Kerstman-Fedor-Kevin/KerstmanPROG6-Fedor-Kevin/Migrations/20230410185219_init.Designer.cs
+++ Kerstman-Fedor-Kevin/KerstmanPROG6-Fedor-Kevin/Migrations/20230410185219_init.Designer.cs
@@ -4,16 +4,18 @@ using KerstmanPROG6_Fedor_Kevin.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace KerstmanPROG6_Fedor_Kevin.Data.Migrations
+namespace KerstmanPROG6_Fedor_Kevin.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230410185219_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
