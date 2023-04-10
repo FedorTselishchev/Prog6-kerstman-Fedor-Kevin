859830ef004993788d9bbd339b2fbfac061975c1
Kevin <kevinskievanherwijnen@gmail.com>
2023-04-10 20:36:17 +0200

    REMOTE DB EN LOGIN WEER GE ADD

--- Kerstman-Fedor-Kevin/KerstmanPROG6-Fedor-Kevin/Program.cs
+++ Kerstman-Fedor-Kevin/KerstmanPROG6-Fedor-Kevin/Program.cs
@@ -1,6 +1,5 @@
 using KerstmanPROG6_Fedor_Kevin.Data;
 using KerstmanPROG6_Fedor_Kevin.models;
-using KerstmanPROG6_Fedor_Kevin.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SantasWishlist.Domain;
@@ -49,5 +48,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-Seeder.Initialize(app);
+
 app.Run();
